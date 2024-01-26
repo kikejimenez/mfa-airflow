53c36c2cc26e6642c60ca4c3669ff9e912b04cbb
ae-mx-10073 <kikejimenez@github.com>
2024-01-26 16:23:49 -0700

    install requirements task

--- dags/bubble_metrics/dbt_bubble_metrics_dag.py
+++ dags/bubble_metrics/dbt_bubble_metrics_dag.py
@@ -11,12 +11,33 @@ default_args = {
 @dag(schedule_interval="@daily", default_args=default_args, catchup=False)
 def dbt_dag():
     @task
+    def install_requirements():
+        import subprocess
+
+        # Path to your requirements.txt file
+        requirements_path = "/opt/requirements.txt"
+
+        # Command to install packages from requirements.txt
+        install_command = f"pip install -r {requirements_path}"
+
+        try:
+            # Execute the command
+            subprocess.run(install_command, check=True, shell=True)
+            print("Successfully installed packages from requirements.txt")
+        except subprocess.CalledProcessError as e:
+            # Handle exceptions if the installation fails
+            print(f"Failed to install packages: {e}")
+
+        # Additional commands can be added here if necessary
+        # ...
+
+    @task
     def run_dbt():
         import subprocess
         import os
 
         # Fetch the dbt project path from env Variables
-        dbt_project_path = os.getenv("DBT_PROJECT_DIR", "/opt/dbt")
+        dbt_project_path = "/opt/dbt"
 
         # Fetch the dbt environment flag from env Variables
         dbt_env = Variable.get("DBT_PROFILE_ENV", default_var="dev")
@@ -29,6 +50,8 @@ def dbt_dag():
 
         subprocess.run(["dbt", "run"], cwd=dbt_project_path, env=env)
 
+    # Use the task in your DAG
+    install_reqs = install_requirements()
     dbt_task = run_dbt()
 
 
---
+++ utils/airflow.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+airflow tasks test dbt_dag run_dbt 2024-01-19
